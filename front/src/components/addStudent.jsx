f8777e64886851e76b5c8cdb09b15ff1f6463691
ad.bme <ab@eva-it.ch>
2023-12-01 14:14:16 +0100

    Ajout d'une api test d'élèves

--- front/src/components/addStudent.jsx
+++ front/src/components/addStudent.jsx
@@ -1,13 +1,15 @@
-
-
-
+import { useEffect, useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const AddStudent = () => {
   
 
   return (
-    <div> 
-      <h2 className='title'>Ajouter un élève</h2>
+    <div>
+      <h2 className="title">Ajouter un élève</h2>
+      <h3>Test api élèves:</h3>
+      
     </div>
   );
 };
