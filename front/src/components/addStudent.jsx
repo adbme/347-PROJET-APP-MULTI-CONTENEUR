d1d58ed6068c31defe486aa150b4e73e73c72de1
ad.bme <ab@eva-it.ch>
2023-12-04 11:10:56 +0100

    Ajout des champs pour post un user, responsiv

--- front/src/components/addStudent.jsx
+++ front/src/components/addStudent.jsx
@@ -1,4 +1,4 @@
-import "../styles/AddStudent.css"
+import "../styles/AddStudent.css";
 import { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -8,33 +8,34 @@ import Alert from 'react-bootstrap/Alert';
 
 const AddStudent = () => {
   const [newStudent, setNewStudent] = useState({
-    name: "",
-    email: "",
-    phone: "",
+    nom: "",
+    prenom: "",
+    entreprise: "",
+    mail: "",
+    num: "",
+    dateNaiss: "",
+    anneeApprentissage: "",
+    statut: false,
   });
 
-  function showSuccess(newStudent){
-    let success = document.getElementById("message")
-    success.innerHTML ="élève "+newStudent + " ajouté"
-    success.className = "success"
+  function showSuccess(newStudent) {
+    let success = document.getElementById("message");
+    success.innerHTML = "Élève " + newStudent + " ajouté";
+    success.className = "success";
     setTimeout(() => {
-      success.className =""
-  }, 3000);
-  
-  }
-  
-  function showError(){
-   let error = document.getElementById("message")
-    error.innerHTML ="Remplissez tout les champs !"
-    error.className = "error"
-    
-    setTimeout(() => {
-      error.className =""
-  }, 3000);
-  
+      success.className = "";
+    }, 3000);
   }
 
-  
+  function showError() {
+    let error = document.getElementById("message");
+    error.innerHTML = "Remplissez tous les champs !";
+    error.className = "error";
+
+    setTimeout(() => {
+      error.className = "";
+    }, 3000);
+  }
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -47,13 +48,26 @@ const AddStudent = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (newStudent.name && newStudent.email && newStudent.phone) {
-      showSuccess(newStudent.name);
+    if (
+      newStudent.nom &&
+      newStudent.prenom &&
+      newStudent.entreprise &&
+      newStudent.mail &&
+      newStudent.num &&
+      newStudent.dateNaiss &&
+      newStudent.anneeApprentissage
+    ) {
+      showSuccess(newStudent.nom);
 
       setNewStudent({
-        name: "",
-        email: "",
-        phone: "",
+        nom: "",
+        prenom: "",
+        entreprise: "",
+        mail: "",
+        num: "",
+        dateNaiss: "",
+        anneeApprentissage: "",
+        statut: false,
       });
     } else {
       showError();
@@ -68,13 +82,35 @@ const AddStudent = () => {
       <Card className="text-center">
         <Card.Body>
           <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formName">
+            <Form.Group controlId="formNom">
               <Form.Label>Nom</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Entrez le nom"
-                name="name"
-                value={newStudent.name}
+                name="nom"
+                value={newStudent.nom}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formPrenom">
+              <Form.Label>Prénom</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Entrez le prénom"
+                name="prenom"
+                value={newStudent.prenom}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formEntreprise">
+              <Form.Label>Entreprise</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Entrez le nom de l'entreprise"
+                name="entreprise"
+                value={newStudent.entreprise}
                 onChange={handleInputChange}
               />
             </Form.Group>
@@ -84,19 +120,43 @@ const AddStudent = () => {
               <Form.Control
                 type="email"
                 placeholder="Entrez l'email"
-                name="email"
-                value={newStudent.email}
+                name="mail"
+                value={newStudent.mail}
                 onChange={handleInputChange}
               />
             </Form.Group>
 
-            <Form.Group controlId="formPhone">
-              <Form.Label>Téléphone</Form.Label>
+            <Form.Group controlId="formNum">
+              <Form.Label>Numéro de téléphone</Form.Label>
               <Form.Control
                 type="tel"
-                placeholder="Entrez le téléphone"
-                name="phone"
-                value={newStudent.phone}
+                placeholder="Entrez le numéro de téléphone"
+                name="num"
+                value={newStudent.num}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formDateNaiss">
+              <Form.Label>Date de naissance</Form.Label>
+              <Form.Control
+                type="date"
+                placeholder="Entrez la date de naissance"
+                name="dateNaiss"
+                value={newStudent.dateNaiss}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formAnneeApprentissage">
+              <Form.Label>Année d'apprentissage</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Entrez l'année d'apprentissage"
+                name="anneeApprentissage"
+                value={newStudent.anneeApprentissage}
+                min={0}
+                max={4}
                 onChange={handleInputChange}
               />
             </Form.Group>
