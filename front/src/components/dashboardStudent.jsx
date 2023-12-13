8ae7a1279a7f2402b6524836bcab3090577175d2
DavisPesce <126652857+DavisPesce@users.noreply.github.com>
2023-12-13 11:36:42 +0100

    dbfront

--- front/src/components/dashboardStudent.jsx
+++ front/src/components/dashboardStudent.jsx
@@ -1,4 +1,3 @@
-
 import "../styles/card.css"
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
@@ -8,51 +7,54 @@ import { faSearch, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useEffect, useState } from "react";
 
-
-
 const DashboardStudent = () => {
     const [students, setStudents] = useState([]);
 
     useEffect(() => {
-      fetch("http://localhost:5000/students")
-        .then((response) => response.json())
-        .then((data) => {
-          setStudents(data);
-          console.log(data);
-        })
-        .catch((error) => console.error("Fetch error:", error));
+        fetch("http://localhost:5000/students")
+            .then((response) => response.json())
+            .then((data) => {
+                setStudents(data);
+                console.log(data);
+            })
+            .catch((error) => console.error("Fetch error:", error));
     }, []);
 
-  return (
-    <div> 
-        <h2 className='title'>Dashboard élèves</h2>
+    return (
+        <div>
+            <h2 className='title'>Dashboard élèves</h2>
 
-        <div className="container">
-        <InputGroup className="mb-3">
-        <InputGroup.Text id="basic-addon1"><FontAwesomeIcon icon={faSearch}  /></InputGroup.Text>
-        <Form.Control
-          placeholder="Rechercher un élève"
-          aria-label="Username"
-          aria-describedby="basic-addon1"
-        />
-      </InputGroup>   
-</div>
+            <div className="container">
+                <InputGroup className="mb-3">
+                    <InputGroup.Text id="basic-addon1"><FontAwesomeIcon icon={faSearch} /></InputGroup.Text>
+                    <Form.Control
+                        placeholder="Rechercher un élève"
+                        aria-label="Username"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+            </div>
 
-<div className="card-container">
-        {students.map((student) => (
-          <Card key={student.id} className="text-center">
-            <Card.Header>{student.name}</Card.Header>
-            <Card.Body>
-              <Card.Text>Email: {student.email}</Card.Text>
-              <Card.Text>Phone: {student.phone}</Card.Text>
-             
-              <Button variant="primary" style={{backgroundColor: "red", border: "none", color: "white"}}><FontAwesomeIcon icon={faTrash}  /> Supprimer</Button>
-            </Card.Body>
-          </Card>
-        ))}
-      </div>
-    </div>
-  );
+            <div className="card-container">
+                {students.map((student) => (
+                    <Card key={student.id} className="text-center">
+                        <Card.Header>{student.nom} {student.prenom}</Card.Header>
+                        <Card.Body>
+                            <Card.Text>Email: {student.mail}</Card.Text>
+                            <Card.Text>Téléphone: {student.num}</Card.Text>
+                            <Card.Text>Entreprise: {student.entreprise}</Card.Text>
+                            <Card.Text>Date de Naissance: {student.dateNaiss}</Card.Text>
+                            <Card.Text>Année d'apprentissage: {student.annee_apprentissage}</Card.Text>
+                            <Card.Text>Statut: {student.statut === 'echec' ? 'Échec' : 'Réussite'}</Card.Text>
+                            <Button variant="primary" style={{backgroundColor: "red", border: "none", color: "white"}}>
+                                <FontAwesomeIcon icon={faTrash} /> Supprimer
+                            </Button>
+                        </Card.Body>
+                    </Card>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default DashboardStudent;
