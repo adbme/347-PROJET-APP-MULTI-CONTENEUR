73f0914a760a5786b3ae769b691c702d36524f9a
ad.bme <ab@eva-it.ch>
2023-12-14 08:39:41 +0100

    t

--- front/src/components/dashboardStudent.jsx
+++ front/src/components/dashboardStudent.jsx
@@ -10,6 +10,20 @@ import { useEffect, useState } from "react";
 const DashboardStudent = () => {
     const [students, setStudents] = useState([]);
 
+    const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+
+app.use(cors()); // Enable CORS for all routes
+
+// Your other routes and middleware
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
     useEffect(() => {
         fetch("http://localhost:5000/students")
             .then((response) => response.json())
